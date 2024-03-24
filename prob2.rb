58e3469f0ef12f3d3e23ff680fe498836176ee67
Shalin Maria <jinx.shalinmaria@gmail.com>
2024-03-23 23:49:43 -0500

    Completed aall problems

---
+++ prob2.rb
@@ -0,0 +1,9 @@
+def valid_password?(password)
+    pattern = /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,16}/
+    password.match?(pattern)
+  end
+  
+  p valid_password?("Passw0rd")
+  p valid_password?("pass")
+
+
